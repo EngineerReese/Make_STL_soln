0a3428bde6959fb1ffc293352cb31a18150ec68a
EngineerReese <eichhorn.r@gmail.com>
2022-12-17 12:06:17 -0500

    stub in web server interface

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    'http://localhost:3000',
+    'localhost:3000',
+]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.get('/', tags=['root'])
 async def root():
     return {}
+
+
+@app.post('/equation')
+async def equation():
+    """
+    Gets an equation from the user to be turned into an STL
+    :return:
+    """
+    return
+
+
+@app.get('/file/{fid}')
+async def stlfile(fid):
+    """
+    downloads a stl file of the identified equation
+    :param fid:
+    :return:
+    """
+    return
--- server.py
+++ server.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
-        port=8432,
+        port=8080,
         reload=True,
     )
